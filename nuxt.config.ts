22b39008ce70c895a1c1088a03795da798adbbd1
Salu Alhassan <alhassansalu1@gmail.com>
2025-03-02 10:17:02 +0000

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
-  compatibilityDate: "2025-02-28"
+  css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      title: 'Data Site',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'this is logistics compay in Ghana. Marketing and Procurement Services. Marketing and supplies of General goods and services. We also help in sourcing of general goods domestically and Internationally.' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
+
+  modules: ['@nuxt/ui'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
+
