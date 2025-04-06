8cddcaa97da6ce307a60e578846dadcc7fcf8716
Sku2626 <alhassansalu1@gmail.com>
2025-04-06 11:35:12 +0000

    update on new feature

--- server/api/accounts.ts
+++ server/api/accounts.ts
@@ -1,7 +1,11 @@
 import { defineEventHandler, createError } from "h3";
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient().$extends(withAccelerate())
+
+// const prisma = new PrismaClient();
 
 export default defineEventHandler(async (event) => {
   try {
